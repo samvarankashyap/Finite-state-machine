a65fc88a7bba838f627aa2a923cf86b9fa936897
SAMVARAN <samvaran@samvaran-laptop>
2013-11-20 14:42:00 +0530

    resolved errors

--- StateMachine.py
+++ StateMachine.py
@@ -6,29 +6,29 @@ class StateMachine:
 		self.alphabet = []
 		self.transitions = []
 
-	def setInitialState(self,initialState):
+	def setInitialState(self, initialState):
 		self.initialState = initialState
 		#print "initial state successfully initialised :P"
 		#print self.initialState
 
-	def addFinalStates(self,finalStates):
+	def addFinalStates(self, finalStates):
 		self.__addListTo(finalStates,"finalStates")
 		#print "final states successfully initialised :P"
 		#print self.finalStates
 
-	def addStates(self,states):
+	def addStates(self, states):
 		self.__addListTo(states,"states")
 		#print "States successfully initialised :P"
 		#print self.states
 
-	def addAlphabet(self,alphabet):
+	def addAlphabet(self, alphabet):
 		self.__addListTo(alphabet,"alphabet")
 
-	def addTransitions(self,transitions):
+	def addTransitions(self, transitions):
 		self.__addListTo(transitions,"transitions")
 		#print "Transitions successfully initialised :P"
 		#print self.transitions
-	def __addListTo(self,list,nameOfList):
+	def __addListTo(self, list, nameOfList):
 		if nameOfList == "finalStates":
 			for temp in list:
 				(self.finalStates).append(temp)
@@ -45,14 +45,14 @@ class StateMachine:
 	def getInitialState(self):
 		return self.initialState[0]
 
-	def stateMachineStart(self,inputString):
+	def stateMachineStart(self, inputString):
 		if (self.validateValues()):
 			currentState = self.getInitialState()
 			finalTransitionState = self.startTransition(inputString,currentState) #  should give final transtition state 
 			if(self.isFinalState(finalTransitionState)):
 				print "Final State Reached ... Transition stopped at "+finalTransitionState
 			else : 
-				print "Transition stopped at "+str(finalTransitionState)
+				print "Invalid String Transition stopped at "+str(finalTransitionState)
 		else :
 			print "object values are not initialised properly ....."
 
@@ -63,29 +63,32 @@ class StateMachine:
 		else :
 			return True
 
-
-	def startTransition(self,inputString,currentState):
+ 
+	def startTransition(self, inputString, currentState):
 		transitionFlow = []
 		transitionFlow.append(self.getInitialState())
 		for c in inputString :
 			nextState = self.getNextTransition(currentState,c)
+			#print nextState
 			if nextState == None :
-				print "transition stopped "
+				#print "transition stopped at "
 				return transitionFlow[-1]
 			currentState = nextState
 			transitionFlow.append(nextState)
-		print transitionFlow
+		#print currentState
+		#print transitionFlow
 		#return transitionFlow
+		return currentState
 
-	def isFinalState(self,someState):
+	def isFinalState(self, someState):
 		isFinal = False
 		for state in self.finalStates:
-			if someState== state:
-				isFinal=True
+			if someState == state:
+				isFinal = True
 		return isFinal
 
 
-	def getNextTransition(self,currentState,letter):
+	def getNextTransition(self, currentState, letter):
 		for t in self.transitions:
 			if t[0]+t[1] == currentState+letter:
 				return t[2]
--- TestStateMachine.py
+++ TestStateMachine.py
@@ -1,8 +1,8 @@
 import StateMachine
 s = StateMachine.StateMachine()
-print s.validateValues()
+#print s.validateValues()
 s.setInitialState("1")
-finalstates = ["6"]
+finalstates = ["5"]
 states = ["1","2","3","4","5"]
 transitions = ["1a2","1b2","2a2","2b1","2c3","3a4","3c5","4a5","4b3"]
 alphabet =["a","b","c"]
@@ -10,10 +10,11 @@ s.addFinalStates(finalstates)
 s.addStates(states)
 s.addAlphabet(alphabet)
 s.addTransitions(transitions)
-print s.getInitialState()
-print s.validateValues()
-print "should come true ",s.isFinalState("6")
-print "should come false  ",s.isFinalState("8")
-print "should print 2 ::::",s.getNextTransition("1","x")
-
-print "start transition function, trainsition ends at ", s.startTransition("xc","1")
+#print s.getInitialState()
+#print s.validateValues()
+#print "should come true ",s.isFinalState("6")
+#print "should come false  ",s.isFinalState("8")
+#print "should print 2 ::::",s.getNextTransition("1","x")
+#print "start transition function,\ntrainsition ends at ", s.startTransition("abcc","1")
+print "start statemachine : "
+s.stateMachineStart("xaac")
